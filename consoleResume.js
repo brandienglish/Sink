f0cf1f283ea362dfcaa6ba3ba846a414f098cde5
brandienglish <59664234+brandienglish@users.noreply.github.com>
2020-01-15 16:50:40 -0600

    Add files via upload

---
+++ consoleResume.js
@@ -0,0 +1,32 @@
+var firstname = 'Name: Brandi English ';
+console.log(firstname)
+
+var two = 'Career: Breathing'
+console.log(two)
+
+var what = 'Description: I am not a fan of Spaghetti ';
+console.log(what)
+
+var ok = 'My interest:'
+console.log(ok)
+
+var interest = ['Eating', 'Learning', 'Funny Videos', 'Traveling'];
+for (i = 0; i < 4; i++) {
+    console.log(interest[i])
+}
+
+var so = 'My Previous Experience:'
+console.log(so)
+
+var info = ['Ambassador for Birmingham Water Works','Clerical Assistant at Fiscal Bereau', 'Cashier at Regal 16'];
+for (i = 0; i < 3; i++) {
+    console.log(info[i])
+}
+
+var skill = 'My Skills:'
+console.log(skill)
+
+var jump =  ['Eating', 'Cooking Good Eggs', 'Collaborating', 'Quickscoping in MW3','Futsal Shuffle Expert'];
+for (i = 0; i < 5; i++) {
+    console.log(jump[i])
+}
