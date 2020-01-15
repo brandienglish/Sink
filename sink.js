1b05b4fe8239a66a0bfac11ff7de3906b870e30b
brandienglish <59664234+brandienglish@users.noreply.github.com>
2020-01-15 08:57:23 -0600

    Add files via upload

---
+++ sink.js
@@ -0,0 +1,12 @@
+var firstname = 'Brandi';
+const states = 50;
+var x = 5; 
+var z = 4;
+var answer = x + z; //adds 5 and 4
+
+function 
+console.log(add(name, age));
+function checkage(name,age){
+    return name + age 
+}
+
